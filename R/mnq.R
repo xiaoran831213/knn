81df5d4d833a739efcfdfa27331f83d717e728c7
xiaoran831213 <tongx1213@163.com>
2018-05-23 19:28:22 -0400

    xiaoran: continue with cpp MINQUE.

--- R/mnq.R
+++ R/mnq.R
@@ -1,14 +1,14 @@
 ## Modified MINQUE
 Modified_MINQUE_Solver <- function(ViList, X, p)
 {
-  A        <- LMM_MINQUE_Solver(ViList = ViList, X = X, p = p)$A;
-  A        <- (A + t(A)) / 2;
-  eigA     <- eigen(A);
-  eigA_val <- pmax(eigA$values,0);
-  eigA_vec <- eigA$vectors;
-  Anew     <- eigA_vec %*% diag(eigA_val) %*% t(eigA_vec);
-  
-  return(Anew)
+    A        <- LMM_MINQUE_Solver(ViList = ViList, X = X, p = p)$A;
+    A        <- (A + t(A)) / 2;
+    eigA     <- eigen(A);
+    eigA_val <- pmax(eigA$values,0);
+    eigA_vec <- eigA$vectors;
+    Anew     <- eigA_vec %*% diag(eigA_val) %*% t(eigA_vec);
+    
+    return(Anew)
 }
 
 
@@ -56,20 +56,21 @@ LMM_MINQUE_Solver <- function(ViList, X, p)
 ## A Helper Function to evaluate r(A,nu) in Infeasible_MINQUE_Newton
 evalR <- function(A, nu, Psi, V, t, p, eps)
 {
-  I        <- diag(rep(1,nrow(A)));
-  invA     <- solve(A+eps*I);
-  r_dual   <- 2*t*c(V %*% A %*% V) - c(invA) + Psi %*% nu;
-  r_primal <- crossprod(Psi, c(A)) - p;
-  r        <- c(r_dual, r_primal)
-  r_norm   <- sqrt(crossprod(r))
-  
-  returnList <- list(r_norm = r_norm, r_dual = r_dual, r_primal = r_primal, invA = invA);
-  return(returnList)
+    I        <- diag(rep(1,nrow(A)));
+    invA     <- solve(A+eps*I);
+    r_dual   <- 2*t*c(V %*% A %*% V) - c(invA) + Psi %*% nu;
+    r_primal <- crossprod(Psi, c(A)) - p;
+    r        <- c(r_dual, r_primal)
+    r_norm   <- sqrt(crossprod(r))
+    
+    returnList <- list(r_norm = r_norm, r_dual = r_dual, r_primal = r_primal, invA = invA);
+    return(returnList)
 }
 
 test <- function()
 {
-    y <- matrix(rnorm(12), 4, 3)
-    x <- rnorm(4)
-    k <- list(e=diag(nrow(y)), p=tcrossprod(x))
+    X <- matrix(rnorm(32), 4, 8)
+    K <- list(e=diag(nrow(X)), p=tcrossprod(X))
+    P <- cbind(diag(length(K)), rep(1, length(K)))
+    .Call('_cpp_mnq', K, P)
 }
--- src/mnq.cpp
+++ src/mnq.cpp
@@ -1,29 +1,78 @@
 // #include <Rcpp.h>
 #include <RcppArmadillo.h>
+#include <vector>
 using namespace Rcpp;
 using namespace arma;
 
 // Cpp implementation of kernel MINQUE (kmq)
 // only explicit kernels, no fixed effect, no implicit product kernels
 // from random effect either.
-SEXP mnq(SEXP _K, SEXP _p)
+RcppExport SEXP mnq(SEXP _K, SEXP _C)
 {
-  List K=as<List>(_K);
-  dvec p=as<dvec>(_p);
+    List K=as<List>(_K);	// kernels / MCVs (matrics of covariance)
+    dmat C=as<dmat>(_C);	// contrast matrix for variance components
 
-  dmat V = as<dmat>(K[0]);
-  int  N = V.n_rows;
-  for(int i=1; i<N; i++)
+    dmat V = as<dmat>(K[0]);	// sum of kernels
+    const int L = K.length();	// number of kernels
+    for(int i=1; i<L; i++)
     {
-      V += as<dmat>(K[i]);
+	V += as<dmat>(K[i]);
     }
-  dmat I(N, N, fill::eye);
 
-  Rcout << "V=" << V << '\n';
-  Rcout << "p=" << p << '\n';
-  Rcout << "N=" << N << '\n';
+    int  N = V.n_rows;		// sample size
+    dmat I(N, N, fill::eye);	// identity kernel (matrix of white noise)
 
-  return(wrap(N));
+    printf("N=%d, L=%d\n", N, L);
+
+    /* Inverse of V -- the sum of kernels:*/
+    dmat W = inv_sympd(V);
+
+    /* Cache the result: B_i = inv(V) K_i inv(V) for i = 1 .. L */
+    List B(L);
+    printf("B.length()=%d\n", B.length());
+    for(int i = 0; i < L; i++)
+    {
+    	B[i] = W * as<dmat>(K[i]) * W;
+    }
+    printf("Done with B\n");
+
+    /* The L-L matrix S */
+    dmat S(L, L);
+    for(int i = 0; i < L; i++)
+    {
+	for(int j=i + 1; j < L; j++)
+	{
+	    S(i, j) = accu(as<dmat>(B[i]) % as<dmat>(K[j]));
+	    S(j, i) = S(i, j);
+	}
+	S(i, i) = accu(as<dmat>(B[i]) % as<dmat>(K[i]));
+    }
+    dmat T = pinv(S);		// Moore-Penrose pseudo-inverse of S
+    dmat R = T * C;		// lambdas
+
+    List A(R.n_cols);
+    for(int j = 0; j < R.n_cols; j++)
+    {
+	dmat x(N, N, fill::zeros);
+	for(int i = 0; i < R.n_rows; i++) // assertion: R.n_rows==L
+	    x += as<dmat>(B[i]); // * R(i, j);
+	A[j] = x;
+    }
+    
+    List ret;
+    ret["B"] = B;
+    ret["K"] = K;
+    ret["N"] = N;
+    ret["L"] = L;
+    ret["C"] = C;
+    ret["V"] = V;
+    ret["W"] = W;
+    ret["S"] = S;
+    ret["T"] = T;
+    ret["R"] = R;
+    ret["A"] = A;
+
+    return(wrap(ret));
 }
 
 // mnq
