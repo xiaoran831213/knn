e7e0cfc3e42f35b5de4aa83e8f0c60d11f79bafc
xiaoran <tongxia1@msu.edu>
2018-09-10 14:06:20 -0400

    xiaoran: change double to float for cpp MINQUE; start working on Eigen MINQUE.

--- src/mnq.cpp
+++ src/mnq.cpp
@@ -1,9 +1,9 @@
 // #include <Rcpp.h>
 #include "RcppArmadillo.h"
-#include <vector>
-#include <numeric>
+
 using namespace Rcpp;
 using namespace arma;
+
 // [[Rcpp::depends(RcppArmadillo)]]
 
 //' Kernel MINQUE
@@ -16,30 +16,32 @@ using namespace arma;
 RcppExport SEXP knl_mnq(SEXP _y, SEXP _V)
 {
     // // [[Rcpp::export]]
-    dmat y = as<dmat>(_y);
+    fmat y = as<fmat>(_y);
     int N = y.n_rows;		// sample size
 
     /* list of kernels */
     List _l = as<List>(_V);	// list of kernels
     int k = _l.length();	// number of kernels
-    std::vector<dmat> V(k);
+    fmat V[k];
     for(int i = 0; i < k; i++)
-	V[i] = as<dmat>(_l[i]);
+	V[i] = as<fmat>(_l[i]);
 
     // Z: sum of kernels (Rao. 1971)
-    dmat Z = std::accumulate(V.begin() + 1, V.end(), V.front());
+    fmat Z(N, N, fill::zeros);
+    for(int i = 0; i < k; i++)
+	Z += V[i];
 
     /* R = Z^{-1} (I - P_v), (Rao. 1971) */
     /* in case of no fixed effect X, project P_v = 0, R = Z^{-1} */
-    dmat R = inv_sympd(Z);
+    fmat R = inv_sympd(Z);
 
     /* W_i = R V_i R, (Rao. 1971) */
-    std::vector<dmat> B(k);
+    fmat B[k];
     for(int i = 0; i < k; i++)
 	B[i] = R * V[i] * R;
 
     /* The k-k matrix S, S_ij = Tr(B_i V_j) */
-    dmat S(k, k);
+    fmat S(k, k);
     for(int i = 0; i < k; i++)
     {
 	for(int j = i + 1; j < k; j++)
@@ -53,28 +55,30 @@ RcppExport SEXP knl_mnq(SEXP _y, SEXP _V)
     /* Lambda_i = P_i' S^{-1} (Rao. 1917), i = 1 .. nrow(P).
        To solve each VC, the contrast matrix must be I(k), so
        Lambda = S^{-1} */
-    dmat L = pinv(S);
-    List A(L.n_rows);		      // k A-matrices
-    dvec W(L.n_rows);		      // k contrasts -> k VC(s)
-    dvec d(N);			      // eigen values of A
-    dmat v(N, N);		      // eigen vectors of A
-    dmat u(N, N);		      // eigen vectors of A
+    // fmat L = pinv(S);
+    fmat L = inv_sympd(S);
+    // List A(L.n_rows);		      // k A-matrices
+    fmat A[L.n_rows];
+    fvec W(L.n_rows);		      // k contrasts -> k VC(s)
+    fvec d(N);			      // eigen values of A
+    fmat v(N, N);		      // eigen vectors of A
+    fmat u(N, N);		      // eigen vectors of A
     for(int i = 0; i < k; i++)
     {
-        // A = sum_{i=1}^k lamda[i, ] (R V[i, ] R)
-	dmat a = B[0] * L(i, 0);
+        // A_i = sum_{j=1}^k lamda[i, j] (R V[i] R)
+	fmat a = B[0] * L(i, 0);
 	for(int j = 1; j < L.n_cols; j++) // L.n_cols == k
 	    a += B[j] * L(i, j);
 
 	// estimate the i th. variance component
-	double w = as_scalar(y.t() * a * y);
-	if(w < 0) 		// modified MINQUE required
+	float w = as_scalar(y.t() * a * y);
+	if(w < 0.0f) 		// modified MINQUE required
 	{
 	    // project A to its nearest in the PSD space
-	    eig_sym(d, v, (a + a.t())/2);
-	    for(int j = v.n_rows - 1; j >=0; j--)
+	    eig_sym(d, v, (a + a.t())/2.0f);
+	    for(int j = v.n_rows - 1; j >= 0; j--)
 	    {
-		if(d[j] > 0)
+		if(d[j] > 0.0f)
 		    u.col(j) = v.col(j) * d[j];
 		else
 		    u.col(j).zeros();
@@ -87,21 +91,21 @@ RcppExport SEXP knl_mnq(SEXP _y, SEXP _V)
     }
 
     /* Standard Error of VC(s), assuming y is normal */
-    dmat C = V[0] * W[0];	// marginal cov of y
-    dvec e(k);
-    for(int i = 1; i < k; i++)
-	C += V[i] * W[i];
+    // fmat C = V[0] * W[0];	// marginal cov of y
+    // fvec e(k);
+    // for(int i = 1; i < k; i++)
+    //  	C += V[i] * W[i];
 
-    // se(s2_i) = 2Tr(A_i C_y A_i C_y)
-    for(int i = 0; i < k; i++)
-	e[i] = 2.0 * accu(pow(as<dmat>(A[i]) * C, 2));
+    /* se(s2_i) = 2Tr(A_i C_y A_i C_y) */
+    // for(int i = 0; i < k; i++)
+    //  	e[i] = 2.0f * accu(square(A[i] * C));
 
     List ret;
-    ret["S"] = S;		// S_ij = Tr[B_i V_j]
-    ret["L"] = L;		// lambda for each contrast
-    ret["A"] = A;		// A matrices
-    ret["C"] = C;
+    // ret["S"] = S;		// S_ij = Tr[B_i V_j]
+    // ret["L"] = L;		// lambda for each contrast
+    // ret["A"] = A;		// A matrices
+    // ret["C"] = C;
     ret["vcs"] = W;		// variance components
-    ret["se2"] = e;		// standard errors
+    ret["se2"] = 1;		// standard errors
     return(Rcpp::wrap(ret));
 }
