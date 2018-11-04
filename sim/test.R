7ddaaa5e3c9b7e67e5b52eaf59b69583c74f1c58
xtong <tongxia1@msu.edu>
2018-11-04 02:53:19 -0500

    xiaoran: create plot for mapping by quantile.

--- sim/test.R
+++ sim/test.R
@@ -65,33 +65,50 @@ test.kpl1 <- function(N=500, P=2000)
 }
 
 source('sim/lnk.R')
-test.lnk <- function()
+test.lnk <- function(hist=0, seed=100)
 {
-    x <- rnorm(50000, 0, 1)
+    set.seed(seed)
+    x <- rnorm(10000, 0, sqrt(2.0))
     dat <- within(list(),
     {
-        NM <- DF(d='nn', x=x, y=x)
         ## st <- DF(d='st', x=x, y=dc(x, 'st', 0.05))
         ## ca <- DF(d='ca', x=x, y=dc(x, 'ca', 0.05))
-        bn <- DF(d='bn', x=x, y=dc(x, 'bn', 0.00))
-        ch <- DF(d='ch', x=x, y=dc(x, 'ch', 0.00))
-        ps <- DF(d='ps', x=x, y=dc(x, 'ps', 0.00))
-        ex <- DF(d='ex', x=x, y=dc(x, 'ex', 0.00))
-        o2 <- DF(d='o2', x=x, y=O2(x))
-        o2 <- DF(d='i2', x=x, y=I2(x))
+        bn <- DF(d='binomial', x=x, y=dc(x, 'bn', 0.00))
+        ch <- DF(d='chi-square', x=x, y=dc(x, 'ch', 0.00))
+        ps <- DF(d='Poisson', x=x, y=dc(x, 'ps', 0.00))
+        ex <- DF(d='exponential', x=x, y=dc(x, 'ex', 0.00))
+        ## o2 <- DF(d='o2', x=x, y=O2(x))
+        ## o3 <- DF(d='i2', x=x, y=O3(x))
     })
-    dat <- do.call(rbind, dat)
-    
-    ## dst <- rbind(
-    ##     DF(dst='nm', val=x),
-    ##     DF(dst='st', val=x.st),
-    ##     DF(dst='ca', val=x.ca))
+    d0 <- do.call(rbind, dat)
 
     library(ggplot2)
     g <- ggplot()
-    ## g <- g + geom_freqpoly(aes(color=dst), binwidth = 0.1)
-    ## g <- g + xlim(-15, 15)
-    ## g <- g + ylim(c(1, NA))
-    g <- g + geom_line(aes(x=x, y=y, color=d), dat)
+    r <- 6
+    g <- g + xlim(-r, +r) + ylim(-r, +r)
+    g <- g + ylab(expression(symbol(eta) %~% symbol(f)))
+    g <- g + xlab(expression(tilde(symbol(eta)) %~% symbol(g)))
+    if(hist == 1)
+        g <- g + geom_histogram(aes(x=x, y=6*..density..), d0, fill='gray', alpha=.5, binwidth=.5)
+
+    g <- g + geom_point(aes(x=x, y=x), d0, color='gray', alpha=.01)
+    g <- g + geom_point(aes(x=y, y=x, color=d), d0, alpha=.01)
+
+    if(hist == 1)
+        g <- g + geom_histogram(aes(x=y, y=6*..density.., fill=d), d0, color='black', binwidth=.5)
+
+    g <- g + coord_flip()
+    g <- g + facet_wrap(~d, 1)
+    g <- g + theme(legend.position = "none",
+                   strip.text=element_text(face='bold', size=15),
+                   strip.background = element_rect(colour="red", fill="#CCCCFF"),
+                   axis.title = element_text(face='bold', size=15))
+    ## axis.title=element_blank()
     g
+}
+
+main <- function()
+{
+    ggsave('~/Dropbox/pub/img/mq2_tran.png', test.lnk(0), scale=1, width=19, height=19/4-.2)
+    ggsave('~/Dropbox/pub/img/mq2_hist.png', test.lnk(1), scale=1, width=19, height=19/4-.2)
 }
